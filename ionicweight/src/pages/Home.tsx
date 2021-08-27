930419a8f1fceae639e06ce0563fd629c3582d6f
felicityandherdragon <felicitywuny@gmail.com>
2021-08-27 00:27:14 -0400

    update to track weight

---
+++ ionicweight/src/pages/Home.tsx
@@ -0,0 +1,63 @@
+import MeasureListItem from '../components/MeasureListItem';
+import { useState } from 'react';
+import { Measure, getMeasures } from '../data/measures';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton
+} from '@ionic/react';
+import './Home.css';
+
+interface HomeProps {
+  measures: Array<Measure>,
+  setMeasures: Function
+}
+
+const Home: React.FC<HomeProps> = ({measures, setMeasures}) => {
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 3000);
+  };
+
+  return (
+    <IonPage id="home-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Inbox</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">
+              Measurements
+            </IonTitle>
+            <IonButtons slot="end">
+              <IonButton routerLink={`/add`}>Add</IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonList>
+          {measures.map(m => <MeasureListItem key={m.id} measure={m} setMeasures={setMeasures}
+  measures={measures}/>)}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
